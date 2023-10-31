230fe35dacf1d7906927e07809202a8f55fc8074
ghdeo <goldencorn7@naver.com>
2023-10-31 11:16:36 +0900

    comment: Add comments

--- indi/indi_test.py
+++ indi/indi_test.py
@@ -4,14 +4,14 @@ from PyQt5.QtCore import *
 from PyQt5.QAxContainer import *
 from PyQt5.QtWidgets import *
 import pandas as pd
-import GiExpertControl as giLogin
-import GiExpertControl as giJongmokTRShow
-import GiExpertControl as giJongmokRealTime
+import GiExpertControl as giLogin # 통신 모듈 로그인할거
+import GiExpertControl as giJongmokTRShow # GiExpertControl은 우리가 파일 넣어줬던거 GiExpertControl.py 이거
+import GiExpertControl as giJongmokRealTime # 
 from TestUI import Ui_MainWindow
 
 main_ui = Ui_MainWindow()
 
-class indiWindow(QMainWindow):
+class indiWindow(QMainWindow): # 
     # UI 선언
     def __init__(self):
         super().__init__()
@@ -26,44 +26,44 @@ class indiWindow(QMainWindow):
         main_ui.pushButton.clicked.connect(self.pushButton_clicked)
         main_ui.pushButton_2.clicked.connect(self.pushButton_2_clicked)
         main_ui.pushButton_3.clicked.connect(self.pushButton_3_clicked)
-        giJongmokTRShow.SetCallBack('ReceiveData', self.giJongmokTRShow_ReceiveData)
-        giJongmokRealTime.SetCallBack('ReceiveRTData', self.giJongmokRealTime_ReceiveRTData)
+        giJongmokTRShow.SetCallBack('ReceiveData', self.giJongmokTRShow_ReceiveData) # 단일조회
+        giJongmokRealTime.SetCallBack('ReceiveRTData', self.giJongmokRealTime_ReceiveRTData) # 실시간조회
         
-        print(giLogin.GetCommState())
-        if giLogin.GetCommState() == 0: # 정상
-            print("")        
-        elif  giLogin.GetCommState() == 1: # 비정상
-        #본인의 ID 및 PW 넣으셔야 합니다.
-            login_return = giLogin.StartIndi('아이디','비밀번호','공인인증서 비밀번호', 'C:\\SHINHAN-i\\indi\\GiExpertStarter.exe')
-            if login_return == True:
-                print("INDI 로그인 정보","INDI 정상 호출")
-            else:
-                print("INDI 로그인 정보","INDI 호출 실패")                    
+        # print(giLogin.GetCommState())
+        # if giLogin.GetCommState() == 0: # 정상
+        #     print("")        
+        # elif  giLogin.GetCommState() == 1: # 비정상
+        # #본인의 ID 및 PW 넣으셔야 합니다.
+        #     login_return = giLogin.StartIndi('아이디','비밀번호','공인인증서 비밀번호', 'C:\\SHINHAN-i\\indi\\GiExpertStarter.exe')
+        #     if login_return == True:
+        #         print("INDI 로그인 정보","INDI 정상 호출")
+        #     else:
+        #         print("INDI 로그인 정보","INDI 호출 실패")                    
 
-    def pushButton_clicked(self):
+    def pushButton_clicked(self): # 위에 버튼
         gaejwa_text = main_ui.textEdit.toPlainText()
-        PW_text = main_ui.textEdit_2.toPlainText()
+        PW_text = main_ui.textEdit_2.toPlainText() # pyqt에 입력된 값들,,
         TR_Name = "SABA200QB"          
         ret = giJongmokTRShow.SetQueryName(TR_Name)          
         # print(giJongmokTRShow.GetErrorCode())
         # print(giJongmokTRShow.GetErrorMessage())
         ret = giJongmokTRShow.SetSingleData(0,gaejwa_text)
         ret = giJongmokTRShow.SetSingleData(1,"01")
-        ret = giJongmokTRShow.SetSingleData(2,PW_text)
-        rqid = giJongmokTRShow.RequestData()
+        ret = giJongmokTRShow.SetSingleData(2,PW_text) # 세팅,,
+        rqid = giJongmokTRShow.RequestData() # 요청
         print(type(rqid))
         print('Request Data rqid: ' + str(rqid))
         self.rqidD[rqid] = TR_Name    
         
     def pushButton_2_clicked(self):      
         jongmokCode = main_ui.textEdit_3.toPlainText()
-        rqid = giJongmokRealTime.RequestRTReg("SC",jongmokCode)
+        rqid = giJongmokRealTime.RequestRTReg("SC",jongmokCode) # SC에 종목을 등록하겠다.
         print(type(rqid))
         print('Request Data rqid: ' + str(rqid))        
         
-    def giJongmokTRShow_ReceiveData(self,giCtrl,rqid):
+    def giJongmokTRShow_ReceiveData(self,giCtrl,rqid): # 맨위 버튼 값 처리
         print("in receive_Data:",rqid)
-        print('recv rqid: {}->{}\n'.format(rqid, self.rqidD[rqid]))
+        print('recv rqid: {}->{}\n'.format(rqid, self.rqidD[rqid])) #
         TR_Name = self.rqidD[rqid]
         tr_data_output = []
         output = []
@@ -91,14 +91,15 @@ class indiWindow(QMainWindow):
             main_ui.tableWidget_2.setItem(final_rowCount,1,QTableWidgetItem(str(giCtrl.GetSingleData(2))))
             main_ui.tableWidget_2.setItem(final_rowCount,2,QTableWidgetItem(str(giCtrl.GetSingleData(3))))
             main_ui.tableWidget_2.setItem(final_rowCount,3,QTableWidgetItem(str(giCtrl.GetSingleData(6))))
-    def pushButton_3_clicked(self):
+            
+    def pushButton_3_clicked(self): # 시세 등록 끄는거.
         giJongmokRealTime.UnRequestRTReg("SC", "")
         
-if __name__ == "__main__":
+if __name__ == "__main__": # 1. 맨처음 메인 탄다.
     app = QApplication(sys.argv)
     IndiWindow = indiWindow()    
     IndiWindow.show()
     app.exec_()
     
-    if IndiWindow.MainSymbol != "":
-        giJongmokRealTime.UnRequestRTReg("SC", "")
+    # if IndiWindow.MainSymbol != "":
+    #     giJongmokRealTime.UnRequestRTReg("SC", "")
